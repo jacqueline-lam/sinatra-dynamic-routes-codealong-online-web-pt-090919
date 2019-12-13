ca97424c325d08b9a68d5740245f63c679a65d3d
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-13 02:39:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,10 +21,8 @@ class App < Sinatra::Base
   
   #GET /multiply/:num1/:num2 displays the product of 2 numbers in the route
   get "/multiply/:num1/:num2" do
-    @num1 = params[:num1]
-    @num2 = params[:num2]
-    "#{@num1}"
-    "#{@num2}"
+    @product = params[:num1] * params[:num2]
+    "The product is #{@products}"
   end
   
 end
